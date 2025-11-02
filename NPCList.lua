097c0056993a5c06c9cc6cedd864c55768c8cdf5
Fayz <JustinLoecher@GMail.com>
2025-11-02 13:07:04 -0600

    Update NPCList.lua

--- NPCList.lua
+++ NPCList.lua
@@ -698,6 +698,7 @@ addToDefaultNpcsToMark(L["Ahn'Qiraj"], "entrance_2", {
   ["0xF130003BA001560F"]  = CIRCLE, -- Anubisath Sentinel
   ["0xF130003BA001560E"]  = SQUARE, -- Anubisath Sentinel
 })
+
 addToDefaultNpcsToMark(L["Ahn'Qiraj"], "entrance_patrols_2", {
   ["0xF130003B9E130799"] = SKULL, -- Obsidian Eradicator
   ["0xF130003B9E01580A"] = CROSS, -- Obsidian Eradicator
@@ -2746,6 +2747,7 @@ addToDefaultNpcsToMark(L["Stratholme"], "dead_pats", {
   -- ["0xF130002A3900D22F"] = STAR,-- Stonespine -- need the mark for eye
   ["0xF1300028AB11E66C"] = STAR,-- Eye of Naxxramas
 })
+
 
 
 
